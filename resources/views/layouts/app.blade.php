7a22a7b0d3c5c282e9bc3a4a418ec7f170b02529
Nick Verheijen <verheijen.webdevelopment@gmail.com>
2020-02-07 15:20:11 +0100

    Homepage now responsive except for mobile navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>NNW @yield('page_title')</title>
         <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
         
