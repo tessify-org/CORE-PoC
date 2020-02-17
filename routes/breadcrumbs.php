b49602e4a50dd62383f8375d656c04031a7b73e2
Nick Verheijen <verheijen.webdevelopment@gmail.com>
2020-02-17 17:10:16 +0100

    Updated breadcrumbs

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -6,75 +6,75 @@
 
 // Home
 Breadcrumbs::for("home", function($t) {
-    $t->push("Home", route("home"));
+    $t->push(__('tessify-core::breadcrumbs.home'), route("home"));
 });
 
 // Register
 Breadcrumbs::for("auth.register", function($t) {
     $t->parent("home");
-    $t->push("Registreren", route("auth.register"));
+    $t->push(__('tessify-core::breadcrumbs.register'), route("auth.register"));
 });
 
 // Login
 Breadcrumbs::for("auth.login", function($t) {
     $t->parent("home");
-    $t->push("Login", route("auth.login"));
+    $t->push(__('tessify-core::breadcrumbs.login'), route("auth.login"));
 });
 
 // Forgot password
 Breadcrumbs::for("auth.forgot-password", function($t) {
     $t->parent("auth.login");
-    $t->push("Wachtwoord vergeten", route("auth.forgot-password"));
+    $t->push(__('tessify-core::breadcrumbs.forgot_password'), route("auth.forgot-password"));
 });
 
 // Reset password
 Breadcrumbs::for("auth.reset-password", function($t, $data) {
     $t->parent("auth.forgot-password");
-    $t->push("Wachtwoord herstellen", route("auth.reset-password", ["code" => $data["code"], "email" => $data["email"]]));
+    $t->push(__('tessify-core::breadcrumbs.reset_password'), route("auth.reset-password", ["code" => $data["code"], "email" => $data["email"]]));
 });
 
 // Search
 Breadcrumbs::for("search", function($t) {
     $t->parent("home");
-    $t->push("Zoeken", route("search"));
+    $t->push(__('tessify-core::breadcrumbs.search'), route("search"));
 });
 
 // Memberlist
 Breadcrumbs::for("memberlist", function($t) {
     $t->parent("home");
-    $t->push("Memberlist", route("memberlist"));
+    $t->push(__('tessify-core::breadcrumbs.memberlist'), route("memberlist"));
 }); 
 
 // Profile
 Breadcrumbs::for("profile", function($t, $user) {
     $t->parent("memberlist");
-    $t->push("Profiel van ".$user->formattedName, route("profile", $user->slug));
+    $t->push(__('tessify-core::breadcrumbs.profile', ['name' => $user->formattedName]), route('profile', $user->slug));
 });
 Breadcrumbs::for("profile.update", function($t, $user) {
     $t->parent("profile", $user);
-    $t->push("Update profiel", route("profile.update"));
+    $t->push(__('tessify-core::breadcrumbs.profile_update'), route("profile.update"));
 });
 
 // Projecten
 Breadcrumbs::for("projects", function($t) {
     $t->parent("home");
-    $t->push("Projecten", route("projects"));
+    $t->push(__('tessify-core::breadcrumbs.projects'), route("projects"));
 });
 Breadcrumbs::for("projects.view", function($t, $project) {
     $t->parent("projects");
-    $t->push("Bekijk project", route("projects.view", $project->slug));
+    $t->push(__('tessify-core::breadcrumbs.projects_view'), route("projects.view", $project->slug));
 });
 Breadcrumbs::for("projects.create", function($t) {
     $t->parent("projects");
-    $t->push("Project toevoegen", route("projects.create"));
+    $t->push(__('tessify-core::breadcrumbs.projects_create'), route("projects.create"));
 });
 Breadcrumbs::for("projects.edit", function($t, $project) {
     $t->parent("projects.view", $project);
-    $t->push("Project aanpassen", route("projects.edit", $project->slug));
+    $t->push(__('tessify-core::breadcrumbs.projects_update'), route("projects.edit", $project->slug));
 });
 Breadcrumbs::for("projects.delete", function($t, $project) {
     $t->parent("projects.view", $project);
-    $t->push("Project verwijderen", route("projects.delete", $project->slug));
+    $t->push(__('tessify-core::breadcrumbs.projects_delete'), route("projects.delete", $project->slug));
 });
 
 //
@@ -83,5 +83,5 @@ Breadcrumbs::for("projects.delete", function($t, $project) {
 
 // Dashboard
 Breadcrumbs::for("admin.dashboard", function($t) {
-    $t->push("Dashboard", route("admin.dashboard"));
+    $t->push(__('tessify-core::breadcrumbs.admin_dashboard'), route("admin.dashboard"));
 });
