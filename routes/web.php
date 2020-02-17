71d3f8839e315134639cc35a83f787483e87ab12
Nick Verheijen <verheijen.webdevelopment@gmail.com>
2020-02-17 14:17:37 +0100

    Moved authentication to CORE package

--- routes/web.php
+++ routes/web.php
@@ -14,27 +14,6 @@
 // Home
 Route::get("/", "HomeController@getHome")->name("home");
 
-// Auth endpoint, when user is logged out
-Route::group(["middleware" => "guest"], function() {
-    
-    // Registration
-    Route::get("register", "Auth\RegisterController@getRegister")->name("auth.register");
-    Route::post("register", "Auth\RegisterController@postRegister")->name("auth.register.post");
-
-    // Login
-    Route::get("login", "Auth\LoginController@getLogin")->name("auth.login");
-    Route::post("login", "Auth\LoginController@postLogin")->name("auth.login.post");
-
-    // Forgot password
-    Route::get("wachtwoord-vergeten", "Auth\ForgotPasswordController@getForgotPassword")->name("auth.forgot-password");
-    Route::post("wachtwoord-vergeten", "Auth\ForgotPasswordController@postForgotPassword")->name("auth.forgot-password.post");
-
-    // Reset password
-    Route::get("wachtwoord-herstellen/{email}/{code}", "Auth\ResetPasswordController@getResetPassword")->name("auth.reset-password");
-    Route::post("wachtwoord-herstellen/{email}/{code}", "Auth\ResetPasswordController@postResetPassword")->name("auth.reset-password.post");
-
-});
-
 // Auth endpoints, when user is logged in
 Route::group(["middleware" => "auth"], function() {
 
