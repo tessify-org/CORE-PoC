40e334b249650146cd5ae6977c2b3062b8021257
Nick Verheijen <verheijen.webdevelopment@gmail.com>
2020-05-15 04:38:24 +0200

    More information about team member roles (view dialog)

--- resources/js/components/projects/view/ProjectTeamMemberOverview.vue
+++ resources/js/components/projects/view/ProjectTeamMemberOverview.vue
@@ -34,7 +34,7 @@
         </div>
 
         <!-- View team member dialog -->
-        <v-dialog v-model="dialogs.view.show" width="500">
+        <v-dialog v-model="dialogs.view.show" width="600">
             <div class="dialog" v-if="dialogs.view.index !== null && mutableMembers[dialogs.view.index] !== undefined">
                 <!-- Close button -->
                 <div class="dialog__close-button" @click="dialogs.view.show = false">
@@ -55,18 +55,34 @@
                             </div>
                         </div>
                         <div class="detail">
-                            <div class="key">{{ strings.view_dialog_roles }}</div>
-                            <div class="val">
-                                <div class="tags">
-                                    <div class="tag-wrapper" v-for="(role, ri) in mutableMembers[dialogs.view.index].team_roles" :key="ri">
-                                        {{ role.name }}
+                            <div class="key">{{ strings.view_dialog_joined_on }}</div>
+                            <div class="val">{{ mutableMembers[dialogs.view.index].created_at }}</div>
+                        </div>
+                    </div>
+                    <!-- Role information -->
+                    <div v-if="mutableMembers[dialogs.view.index].team_roles.length > 0">
+                        <!-- Subtitle -->
+                        <h4 class="dialog-subtitle">{{ strings.view_dialog_roles }}</h4>
+                        <!-- Roles -->
+                        <div id="dialog-member-roles">
+                            <div class="member-role" v-for="(role, ri) in mutableMembers[dialogs.view.index].team_roles" :key="ri">
+                                <div class="member-role__title">
+                                    {{ role.name }}
+                                </div>
+                                <div class="member-role__description">
+                                    {{ role.description }}
+                                </div>
+                                <div class="member-role__skills" v-if="role.skills !== undefined && role.skills !== null && role.skills.length > 0">
+                                    <div class="member-role__skills-label">{{ strings.view_dialog_required_skills }}</div>
+                                    <div class="member-role__skills-list">
+                                        <div class="role-skill__wrapper" v-for="(skill, si) in role.skills" :key="si">
+                                            <div class="role-skill">
+                                                {{ skill.name }}
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="detail">
-                            <div class="key">{{ strings.view_dialog_joined_on }}</div>
-                            <div class="val">{{ mutableMembers[dialogs.view.index].created_at }}</div>
                         </div>
                     </div>
                 </div>
@@ -200,6 +216,51 @@
         }
         #project-team-member-overview__no-team-members {
 
+        }
+    }
+    #dialog-member-roles {
+        border-radius: 3px;
+        border: 1px solid rgba(0, 0, 0, 0.1);
+        .member-role {
+            padding: 15px;
+            box-sizing: border-box;
+            border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+            &:last-child {
+                border-bottom: 0;
+            }
+            .member-role__title {
+                font-size: 1.2em;
+                font-weight: 500;
+                margin: 0 0 5px 0;
+            }
+            .member-role__description {
+                font-size: .9em;
+                margin: 0 0 10px 0;
+            }
+            .member-role__skills {
+                .member-role__skills-label {
+                    font-weight: 500;
+                    margin: 0 0 5px 0;
+                }
+                .member-role__skills-list {
+                    display: flex;
+                    flex-wrap: wrap;
+                    flex-direction: row;
+                    margin: 0 -5px -5px -5px;
+                    .role-skill__wrapper {
+                        box-sizing: border-box;
+                        padding: 0 5px 5px 5px;
+                        .role-skill {
+                            color: #fff;
+                            font-size: .8em;
+                            padding: 2px 5px;
+                            border-radius: 2px;
+                            box-sizing: border-box;
+                            background-color: #111;
+                        }
+                    }
+                }
+            }
         }
     }
 </style>
